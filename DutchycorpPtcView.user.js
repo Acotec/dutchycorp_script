e56598f8b1ce07ecbc7cd94f94eaba147aea6725
HARFHO <Harfho77@gmail.com>
2022-03-11 09:31:01 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -1,17 +1,18 @@
 (function() {
-    'use strict';
-      function waitForKeyElements(t, o, e, i, n) {
-        void 0 === e && (e = !0), void 0 === i && (i = 300), void 0 === n && (n = -1);
-        var r = "function" == typeof t ? t() : document.querySelectorAll(t),
-            u = r && 0 < r.length;
-        u && r.forEach(function (t) {
-            var e = "data-userscript-alreadyFound";
-            t.getAttribute(e) || !1 || (o(t) ? u = !1 : t.setAttribute(e, !0))
-        }), 0 === n || u && e || (--n, setTimeout(function () {
-            waitForKeyElements(t, o, e, i, n)
-        }, i))
-    }
-    waitForKeyElements('#ptc-submit-btn',(e)=>{e.click()})
+    'use strict'
+    let progress = setInterval(()=>{
+        console.log('waiting')
+        let i=0
+        let style = document.querySelector("div.progress").getAttribute('style')
+        if(/display: none/ig.test(style)){
+            clearInterval(progress)
+            document.querySelector("#ptc-submit-btn").click()
+        }
+        if(i>61){
+            clearInterval(progress)
+        }
+    },1000)
+    //document.querySelector("div.progress").getAttribute('style')
+    //waitForKeyElements('#ptc-submit-btn',(e)=>{e.click()})
     //document.querySelector("#ptc-submit-btn").click()
-
 })();
