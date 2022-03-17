ca3382862fbe9020a895340c440e25242d25d78c
HARFHO <Harfho77@gmail.com>
2022-03-17 08:42:23 +0100

    Update DutchyCorp(YourBalance).user.js

--- DutchyCorp(YourBalance).user.js
+++ DutchyCorp(YourBalance).user.js
@@ -1,14 +1,19 @@
-(function() {
-    'use strict';
-    let coin_code = 'USDT'
-    let id = '#withdraw_'+coin_code
-    function waitForKeyElements(t,o,e,i,n){void 0===e&&(e=!0),void 0===i&&(i=300),void 0===n&&(n=-1);var r="function"==typeof t?t():document.querySelectorAll(t),u=r&&0<r.length;u&&r.forEach(function(t){var e="data-userscript-alreadyFound";t.getAttribute(e)||!1||(o(t)?u=!1:t.setAttribute(e,!0))}),0===n||u&&e||(--n,setTimeout(function(){waitForKeyElements(t,o,e,i,n)},i))}
-    waitForKeyElements(id, (element) => {
-        element.scrollIntoView();
-    });
-    waitForKeyElements('#withdraw-btn-USDT', (element) => {
-        element.scrollIntoView();
-        element.click()
-    });
-    //document.querySelector("#withdraw_USDT").scrollIntoView();
-})();
+let coin_code = 'USDT'
+    var i=0
+    var limit=10
+    var check = setInterval(()=>{
+        i++
+        var element
+        console.log("waiting for",coin_code,"withdraw button to be available within",limit-i,"seconds")
+        try{
+            element =document.querySelector('#status_text_USDT'+coin_code)
+            if(element){
+                element.scrollIntoView();
+                //element.click()
+            }
+        }catch(e){element=null}
+        if(i>=limit){
+            console.log("waiting for",coin_code,"withdraw button Timeout")
+            clearInterval(check)
+        }
+    },1000)
