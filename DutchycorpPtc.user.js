530ace556c98c83f754d5c619fa4190432d59d28
HARFHO <Harfho77@gmail.com>
2022-04-23 02:47:27 +0100

    Update DutchycorpPtc.user.js

--- DutchycorpPtc.user.js
+++ DutchycorpPtc.user.js
@@ -1,5 +1,13 @@
 var element;
-if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+const pageAccessedByReload = (
+    (window.performance.navigation && window.performance.navigation.type === 1) ||
+    window.performance
+    .getEntriesByType('navigation')
+    .map((nav) => nav.type)
+    .includes('reload')
+);
+
+if (pageAccessedByReload) {
     console.log("This page is reloaded");
     GM_getValue("canclose", false)
 }
