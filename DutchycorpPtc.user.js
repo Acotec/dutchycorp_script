f723721b0693f33716ec4f24291b6963fa4015a0
HARFHO <Harfho77@gmail.com>
2022-05-01 12:34:38 +0100

    Update DutchycorpPtc.user.js

--- DutchycorpPtc.user.js
+++ DutchycorpPtc.user.js
@@ -1,4 +1,8 @@
-try{document.querySelector("#methods > p").scrollIntoView()}catch(e){document.querySelector("#methods > p").scrollIntoViewIfNeeded()}
+try {
+    document.querySelector("#methods > p").scrollIntoView()
+} catch (e) {
+    document.querySelector("#methods > p").scrollIntoViewIfNeeded()
+}
 var element;
 const pageAccessedByReload = (
     (window.performance.navigation && window.performance.navigation.type === 1) ||
@@ -58,98 +62,97 @@ try {
 }
 if (element) {
     window.close()
-} else {
-    SpeedCtr(document.querySelector("center:nth-child(7) > p"))
-    var button = document.createElement("button"),
-        _ptc_ToVisit = Array.from(document.getElementsByClassName("btn-small waves-effect")),
-        _ptc_ToClaim = _ptc_ToVisit.filter((e) => {
-            if (e.name) {
-                return e
-            }
-        }),
-        rewards = Array.from(document.querySelectorAll("b:nth-child(5)")),
-        sum = 0,
-        body = document.querySelector("center:nth-child(7) > p");
-    rewards.forEach((reward) => {
-        sum += parseInt(reward.textContent)
-    })
-    var second_parag = document.createElement("p");
-    second_parag.setAttribute('class', 'title')
-    second_parag.innerText = 'Total Rewards ' + sum
-    body.append(second_parag);
-    body.appendChild(button);
-    button.innerHTML = "Script Not Running"
-    button.addEventListener("click", function() {
-        GM_setValue("canclose", true);
-        button.innerHTML = "Script Run"
-        console.log("Script Run")
-        visitPtc()
+}
+SpeedCtr(document.querySelector("center:nth-child(7) > p"))
+var button = document.createElement("button"),
+    _ptc_ToVisit = Array.from(document.getElementsByClassName("btn-small waves-effect")),
+    _ptc_ToClaim = _ptc_ToVisit.filter((e) => {
+        if (e.name) {
+            return e
+        }
+    }),
+    rewards = Array.from(document.querySelectorAll("b:nth-child(5)")),
+    sum = 0,
+    body = document.querySelector("center:nth-child(7) > p");
+rewards.forEach((reward) => {
+    sum += parseInt(reward.textContent)
+})
+var second_parag = document.createElement("p");
+second_parag.setAttribute('class', 'title')
+second_parag.innerText = 'Total Rewards ' + sum
+body.append(second_parag);
+body.appendChild(button);
+button.innerHTML = "Script Not Running"
+button.addEventListener("click", function() {
+    GM_setValue("canclose", true);
+    button.innerHTML = "Script Run"
+    console.log("Script Run")
+    visitPtc()
+});
+attachHandler([].slice.call(document.getElementsByTagName('a')));
+
+setMutationHandler(document, 'a', function(nodes) {
+    attachHandler(nodes);
+    return true;
+});
+
+function attachHandler(nodes) {
+    nodes.forEach(function(node) {
+        if (node.target != '_blank') {
+            node.onclick = clickHandler;
+            node.addEventListener('click', clickHandler);
+        }
     });
-    attachHandler([].slice.call(document.getElementsByTagName('a')));
+}
 
-    setMutationHandler(document, 'a', function(nodes) {
-        attachHandler(nodes);
-        return true;
-    });
+function clickHandler(e) {
+    if (e.button > 1)
+        return;
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    // GM_openInTab(this.href, e.button || e.ctrlKey);
+    GM_openInTab(this.href, true);
+}
 
-    function attachHandler(nodes) {
-        nodes.forEach(function(node) {
-            if (node.target != '_blank') {
-                node.onclick = clickHandler;
-                node.addEventListener('click', clickHandler);
-            }
-        });
-    }
+function clickOnEle(el) {
+    var simulateMouseEvent = function(element, eventName, coordX, coordY) {
+        element.dispatchEvent(new MouseEvent(eventName, {
+            //view: window,
+            bubbles: true,
+            cancelable: true,
+            clientX: coordX,
+            clientY: coordY,
+            button: 0
+        }));
+    };
+    var theButton = el;
+    var box = theButton.getBoundingClientRect(),
+        coordX = box.left + (box.right - box.left) / 2,
+        coordY = box.top + (box.bottom - box.top) / 2;
+    simulateMouseEvent(theButton, "mousedown", coordX, coordY);
+    simulateMouseEvent(theButton, "mouseup", coordX, coordY);
+    simulateMouseEvent(theButton, "click", coordX, coordY);
+}
 
-    function clickHandler(e) {
-        if (e.button > 1)
-            return;
-        e.preventDefault();
-        e.stopPropagation();
-        e.stopImmediatePropagation();
-        // GM_openInTab(this.href, e.button || e.ctrlKey);
-        GM_openInTab(this.href, true);
-    }
+function visitPtc() {
+    var timerId1;
+    let delay = 0;
+    let timerId = setTimeout(function request() {
+        let visit = _ptc_ToClaim.splice(0, 1)[0];
+        if (visit) {
+            console.log(visit.parentElement.parentElement.parentElement.getElementsByTagName('p')[0].textContent)
+            clickOnEle(visit)
+            delay = GM_getValue('speed', 5) * 1000
+        } else {
+            GM_setValue("canclose", false);
+            clearTimeout(timerId)
+            clearTimeout(timerId1)
+            button.innerHTML = "Done Running Script"
+            console.log("Done Running Script")
+        }
+        console.log(delay)
+        timerId1 = setTimeout(request, delay);
 
-    function clickOnEle(el) {
-        var simulateMouseEvent = function(element, eventName, coordX, coordY) {
-            element.dispatchEvent(new MouseEvent(eventName, {
-                //view: window,
-                bubbles: true,
-                cancelable: true,
-                clientX: coordX,
-                clientY: coordY,
-                button: 0
-            }));
-        };
-        var theButton = el;
-        var box = theButton.getBoundingClientRect(),
-            coordX = box.left + (box.right - box.left) / 2,
-            coordY = box.top + (box.bottom - box.top) / 2;
-        simulateMouseEvent(theButton, "mousedown", coordX, coordY);
-        simulateMouseEvent(theButton, "mouseup", coordX, coordY);
-        simulateMouseEvent(theButton, "click", coordX, coordY);
-    }
-
-    function visitPtc() {
-        var timerId1;
-        let delay = 0;
-        let timerId = setTimeout(function request() {
-            let visit = _ptc_ToClaim.splice(0, 1)[0];
-            if (visit) {
-                console.log(visit.parentElement.parentElement.parentElement.getElementsByTagName('p')[0].textContent)
-                clickOnEle(visit)
-                delay = GM_getValue('speed', 5) * 1000
-            } else {
-                GM_setValue("canclose", false);
-                clearTimeout(timerId)
-                clearTimeout(timerId1)
-                button.innerHTML = "Done Running Script"
-                console.log("Done Running Script")
-            }
-            console.log(delay)
-            timerId1 = setTimeout(request, delay);
-
-        }, delay);
-    }
+    }, delay);
 }
