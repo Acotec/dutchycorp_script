b6eaf7fa3ffe0f66ef020a0520e822c1e685b127
HARFHO <Harfho77@gmail.com>
2022-05-05 08:26:38 +0100

    Update PageLoaded.user.js

--- PageLoaded.user.js
+++ PageLoaded.user.js
@@ -1,14 +1,1 @@
-function tempAlert(msg, duration) {
-    var el = document.createElement("div");
-    el.setAttribute("style", "position:absolute;top:100%;left:20%;background-color:white;");
-    el.innerHTML = msg;
-    setTimeout(function() {
-        el.parentNode.removeChild(el);
-    }, duration);
-    document.body.appendChild(el);
-}
-function show() {
-    document.title = document.title + " (Page Loaded)";
-    tempAlert('Page Loaded', 5000)
-}
-window.addEventListener('load', show, false);
+function show() {document.title = document.title + " (Page Loaded)";alert('PageLoaded')};window.addEventListener('load', show, false);
