ee8b1443c561e1064c61497205f320a5a9e49955
HARFHO <Harfho77@gmail.com>
2022-05-08 04:28:37 +0100

    Update PageLoaded.user.js

--- PageLoaded.user.js
+++ PageLoaded.user.js
@@ -1,1 +1,21 @@
-function show() {document.title = document.title + " (Page Loaded)";};window.addEventListener('load', show, false);
+function show() {
+    let title = document.title + " (Page Loaded)"
+    document.title = title
+    let timer = (x) => {
+        if (x == 0) {
+            return
+        };
+        document.title = title
+        return setTimeout(() => {
+            timer(--x)
+        }, 200)
+    }
+    setTimeout(() => {
+        if (/.*:.*roll.*/ig.test(document.title)) {
+            timer(300)
+        } else {
+            document.title = title
+        }
+    }, 1500)
+};
+window.addEventListener('load', show, false);
