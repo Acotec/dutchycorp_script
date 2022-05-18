1f41bd286a6b03e3432b13e6690eb615cfff7c2c
HARFHO <Harfho77@gmail.com>
2022-05-18 18:46:20 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -1,19 +1,34 @@
 (function() {
     'use strict'
-    var i=0
-    let progress = setInterval(()=>{
+    var i = 0
+    let progress = setInterval(() => {
         i++;
-        console.log('waiting',i)
+        console.log('waiting', i)
         var style;
-        try{style = document.querySelector("div.progress").getAttribute('style')}catch(e){style=null}
-        if(/display: none/ig.test(style)){
-            clearInterval(progress)
-            try{document.querySelector("#ptc-submit-btn").click()}catch(e){document.getElementsByTagName('button')[0].click()}
+        try {
+            style = document.querySelector("div.progress").getAttribute('style')
+        } catch (e) {
+            style = null
         }
-        if(i>=61){clearInterval(progress)}
+        if (/display: none/ig.test(style)) {
+            clearInterval(progress)
+            try {
+                document.querySelector("#ptc-submit-btn").click()
+            } catch (e) {
+                document.getElementsByTagName('button')[0].click()
+            }
+        }
+        if (/All Available Ads Watched/ig.test(document.querySelector("body > div.column.middle > div.row > div.col.s12.m12.l10 > center:nth-child(1) > h4").textContent)) {
+            clearInterval(progress)
+        }
+        if (i > 70) {
+            clearInterval(progress);
+            window.location.reload()
+        }
+    }, 1000)
 
-    },1000)
     //document.querySelector("div.progress").getAttribute('style')
     //waitForKeyElements('#ptc-submit-btn',(e)=>{e.click()})
     //document.querySelector("#ptc-submit-btn").click()
-    })();
+
+})();
