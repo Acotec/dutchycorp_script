5036d09e9397c4dee4eeef71d3536edcc81e2331
HARFHO <Harfho77@gmail.com>
2022-05-19 05:35:19 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -1,34 +1,29 @@
-(function() {
-    'use strict'
-    var i = 0
-    let progress = setInterval(() => {
-        i++;
-        console.log('waiting', i)
-        var style;
+var i = 0,
+    watched;
+let progress = setInterval(() => {
+    i++;
+    console.log('waiting', i)
+    var style;
+    try {
+        style = document.querySelector("div.progress").getAttribute('style')
+    } catch (e) {
+        style = null
+    }
+    if (/display: none/ig.test(style)) {
+        clearInterval(progress)
         try {
-            style = document.querySelector("div.progress").getAttribute('style')
+            document.querySelector("#ptc-submit-btn").click()
         } catch (e) {
-            style = null
+            document.getElementsByTagName('button')[0].click()
         }
-        if (/display: none/ig.test(style)) {
-            clearInterval(progress)
-            try {
-                document.querySelector("#ptc-submit-btn").click()
-            } catch (e) {
-                document.getElementsByTagName('button')[0].click()
-            }
-        }
-        if (/All Available Ads Watched/ig.test(document.querySelector("body > div.column.middle > div.row > div.col.s12.m12.l10 > center:nth-child(1) > h4").textContent)) {
-            clearInterval(progress)
-        }
-        if (i > 70) {
-            clearInterval(progress);
-            window.location.reload()
-        }
-    }, 1000)
-
-    //document.querySelector("div.progress").getAttribute('style')
-    //waitForKeyElements('#ptc-submit-btn',(e)=>{e.click()})
-    //document.querySelector("#ptc-submit-btn").click()
-
-})();
+    };
+    try {
+        watched = document.querySelector("body div.column div.col center:nth-child(1) h4")
+    } catch (e) {}
+    if (/All Available Ads Watched/ig.test(watched.textContent)) {
+        clearInterval(progress)
+    } else if (i > 70) {
+        clearInterval(progress);
+        window.location.reload()
+    }
+}, 1000)
