bdc589ea33e5294def30b2be92c92c8c90252cdf
HARFHO <Harfho77@gmail.com>
2022-05-22 09:23:56 +0100

    Update DutchFpAddressUpdate.user.js

--- DutchFpAddressUpdate.user.js
+++ DutchFpAddressUpdate.user.js
@@ -1,5 +1,5 @@
 (function() {
-       function waitForKeyElements(t,o,e,i,n){void 0===e&&(e=!0),void 0===i&&(i=300),void 0===n&&(n=-1);var r="function"==typeof t?t():document.querySelectorAll(t),u=r&&0<r.length;u&&r.forEach(function(t){var e="data-userscript-alreadyFound";t.getAttribute(e)||!1||(o(t)?u=!1:t.setAttribute(e,!0))}),0===n||u&&e||(--n,setTimeout(function(){waitForKeyElements(t,o,e,i,n)},i))}
+    function waitForKeyElements(t,o,e,i,n){void 0===e&&(e=!0),void 0===i&&(i=300),void 0===n&&(n=-1);var r="function"==typeof t?t():document.querySelectorAll(t),u=r&&0<r.length;u&&r.forEach(function(t){var e="data-userscript-alreadyFound";t.getAttribute(e)||!1||(o(t)?u=!1:t.setAttribute(e,!0))}),0===n||u&&e||(--n,setTimeout(function(){waitForKeyElements(t,o,e,i,n)},i))}
     function getEmail(){
         window.location.href="https://autofaucet.dutchycorp.space/account.php"
     }
@@ -22,8 +22,10 @@
             let email = document.querySelector("#addresses_form > div:nth-child(3)").querySelector('input').value
             if(email){
                 GM_deleteValue('email')
+                console.log('FaucetPay Email already added')
                 clearInterval(check)
             }else{
+                GM_setValue('updateFpAddress',true)
                 updateFpAddress( document.querySelector("#addresses_form > div:nth-child(3)"))
                 clearInterval(check)
             }
@@ -36,7 +38,18 @@
     if(/account.php.*/ig.test(window.location)){
         let email=document.querySelector("#methods > form > font > form:nth-child(10) > div:nth-child(1) > input").value
         GM_setValue('email',email)
-        window.location.href="https://autofaucet.dutchycorp.space/addresses_update.php"
-    }
-    //document.querySelector("#addresses_form > div:nth-child(3)")
+        if(GM_getValue('updateFpAddress')){window.location.href="https://autofaucet.dutchycorp.space/addresses_update.php"}        
+        // else{
+        //     var captcha='recaptcha'
+        //     document.querySelector("#methods > form > font > center:nth-child(12) > h3").scrollIntoView(true)
+        //     Array.from(document.getElementsByClassName('dropdown-content select-dropdown')[0].querySelectorAll("[id*='select-options']")).forEach((li)=>{
+        //         if(GM_getValue('captcha_changed',false)==false&&new RegExp(captcha,'ig').test(li.textContent)){
+        //             li.click()
+        //             GM_setValue('captcha_used',captcha);
+        //             document.getElementsByName('change-captcha')[0].click();
+        //             GM_setValue('captcha_changed',true)
+        //         }
+        //     })
+        // }
+    }    
 })();
