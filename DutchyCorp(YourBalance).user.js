e8bc63f76e67fefb7400631f65afcac03bef6482
HARFHO <Harfho77@gmail.com>
2022-06-05 18:29:26 +0100

    Update DutchyCorp(YourBalance).user.js

--- DutchyCorp(YourBalance).user.js
+++ DutchyCorp(YourBalance).user.js
@@ -1,8 +1,26 @@
 (function(){
-    'use strict';
+'use strict';
+    function waitForKeyElements(t, o, e, i, n) {
+        void 0 === e && (e = !0), void 0 === i && (i = 300), void 0 === n && (n = -1);
+        var r = "function" == typeof t ? t() : document.querySelectorAll(t),
+            u = r && 0 < r.length;
+        u && r.forEach(function(t) {
+            var e = "data-userscript-alreadyFound";
+            t.getAttribute(e) || !1 || (o(t) ? u = !1 : t.setAttribute(e, !0))
+        }), 0 === n || u && e || (--n, setTimeout(function() {
+            waitForKeyElements(t, o, e, i, n)
+        }, i))
+    }
     let coin_code = 'USDT'
     var i=0
     var limit=10
+    waitForKeyElements("#toast-container > div", (element) => {
+        let WithButton=document.getElementById('withdraw-btn2-'+coin_code)
+        let addButton = document.createElement("p")
+        addButton.setAttribute('class', 'title')
+        addButton.innerText =element.innerText
+        WithButton.parentNode.insertBefore(addButton, WithButton.nextSibling);
+    },false);
     var check = setInterval(()=>{
         i++
         var element
