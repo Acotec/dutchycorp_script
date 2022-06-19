9d15a1d65836f4293ad8656ecd0dd00d23a182e8
HARFHO <Harfho77@gmail.com>
2022-06-19 14:53:35 +0100

    Update Dutchycorp_ip.user.js

--- Dutchycorp_ip.user.js
+++ Dutchycorp_ip.user.js
@@ -1,6 +1,5 @@
 (function() {
-    'use strict';
-    try{document.querySelector("#dropdown1 > li:nth-child(10) > a").remove()}catch(err){}//remove logout button
+    'use strict';    
     if(/.*is Linked to your IP.*|what can i do to prevent this in the future|.*Sorry, you have been blocked.*/ig.test(document.body.textContent)){
         window.location.reload(true)
     }
