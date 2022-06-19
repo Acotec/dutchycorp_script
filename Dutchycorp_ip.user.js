ee3350abf787c6537e161ffaff133d0b6c882b23
HARFHO <Harfho77@gmail.com>
2022-06-19 14:53:11 +0100

    Update Dutchycorp_ip.user.js

--- Dutchycorp_ip.user.js
+++ Dutchycorp_ip.user.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     try{document.querySelector("#dropdown1 > li:nth-child(10) > a").remove()}catch(err){}//remove logout button
-    if(/.*is Linked to your IP.*/ig.test(document.body.textContent)){
+    if(/.*is Linked to your IP.*|what can i do to prevent this in the future|.*Sorry, you have been blocked.*/ig.test(document.body.textContent)){
         window.location.reload(true)
     }
 })();
