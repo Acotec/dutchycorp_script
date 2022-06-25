7d1e12df148f1887e74d88d9728d334a1168aac9
HARFHO <Harfho77@gmail.com>
2022-06-25 13:24:29 +0100

    Update DutchycorpPtcView.user.js

--- DutchycorpPtcView.user.js
+++ DutchycorpPtcView.user.js
@@ -1,38 +1,103 @@
-'use strict'
-// var i=0
-// var watched;
-// var progress = setInterval(()=>{
-//     i++;
-//     console.log('waiting',i)
-//     var style;
-//     try{style = document.querySelector("div.progress").getAttribute('style')}catch(e){style=null}
-//     if(/display: none/ig.test(style)){
-//         clearInterval(progress)
-//         try{document.querySelector("#ptc-submit-btn").click()}catch(e){document.getElementsByTagName('button')[0].click()}
-//     };
-//     try{watched = document.querySelector("body div.column div.col center:nth-child(1) h4")}catch(e){};
-//     if(watched&&/.*All.+Ads.+Watched.*/ig.test(watched.textContent)){clearInterval(progress)};
-//     if(i>70){clearInterval(progress);window.location.reload()};
-// },1000)
-
+var i = 0
+var watched;
 let count = 0
-let view = setInterval(() => {
-    var watched
-    try {
-        watched = document.querySelector("body div.column div.col center:nth-child(1) h4")
-    } catch (e) {}
-    if (watched && /.*All.+Ads.+Watched.*/ig.test(watched.textContent)) {
-        clearInterval(view)
-    } else {
-        count++
-        let value = $('.g-recaptcha')[0].name.replace(/.*btn-/ig, '');
-        $('#submit_captcha').show();
-        $('#submit-btn')[0].innerHTML = `<input required type="hidden" name="hash" value="${value}" />`;
-        $('.progress').hide();
-        $('#sec').hide();
-        $('.g-recaptcha').click()
-       if($('#rc-imageselect').length>=1||$('.rc-audiochallenge-tabloop-begin').length>=1) {clearInterval(view)}
-       else if (count >= 10) {clearInterval(view);window.location.reload(true)}
-       //clearInterval(view)
+let value;
+
+function manual() {
+    var progress = setInterval(() => {
+        i++;
+        console.log('waiting', i)
+        var style;
+        try {
+            style = document.querySelector("div.progress").getAttribute('style')
+        } catch (e) {
+            style = null
+        }
+        if (/display: none/ig.test(style)) {
+            clearInterval(progress)
+            try {
+                document.querySelector("#ptc-submit-btn").click()
+            } catch (e) {
+                document.getElementsByTagName('button')[0].click()
+            }
+        };
+        try {
+            watched = document.querySelector("body div.column h4")
+        } catch (e) {};
+        if (watched && /.*All.+Ads.*/ig.test(watched.textContent)) {
+            clearInterval(progress)
+        };
+        if (i > 70) {
+            clearInterval(progress);
+            window.location.reload()
+        };
+    }, 1000)
+}
+
+function auto() {
+    let view = setInterval(() => {
+        try {
+            watched = document.querySelector("body div.column h4")
+        } catch (e) {}
+        if (watched && /.*All.+Ads.*/ig.test(watched.textContent)) {
+            clearInterval(view);
+            window.close()
+        } else {
+            count++
+            $(".g-recaptcha") && (value = $(".g-recaptcha")[0].name.replace(/.*btn-/ig, ""));
+            $("#submit_captcha") && $("#submit_captcha").show();
+            $("#submit-btn") && ($("#submit-btn")[0].innerHTML = `<input required type="hidden" name="hash" value="${value}" />`);
+            $('.progress') && $('.progress').hide();
+            $('#sec') && $('#sec').hide();
+            $('.g-recaptcha') && $('.g-recaptcha').click()
+            if ($('#rc-imageselect') || $('.rc-audiochallenge-tabloop-begin')) {
+                clearInterval(view)
+            } else if (count >= 20) {
+                clearInterval(view);
+                window.location.reload(true)
+            }
+            //clearInterval(view)
+        }
+    }, 1000)
+}
+
+function autotimer() {
+    var title = document.title
+    let timer = (x) => {
+        if (x < 0) {
+            clearInterval(timer);
+            window.location.reload(true);
+            //window.close()
+        };
+        document.title = x + '--' + title;
+        try {
+            watched = document.querySelector("body div.column h4")
+        } catch (e) {}
+        if (watched && /.*All.+Ads.*/ig.test(watched.textContent)) {
+            clearInterval(view);
+            window.close()
+        } else {
+            count++
+            $(".g-recaptcha") && (value = $(".g-recaptcha")[0].name.replace(/.*btn-/ig, ""));
+            $("#submit_captcha") && $("#submit_captcha").show();
+            $("#submit-btn") && ($("#submit-btn")[0].innerHTML = `<input required type="hidden" name="hash" value="${value}" />`);
+            $('.progress') && $('.progress').hide();
+            $('#sec') && $('#sec').hide();
+            $('.g-recaptcha') && $('.g-recaptcha').click()
+            if ($('#rc-imageselect') || $('.rc-audiochallenge-tabloop-begin')) {
+                clearInterval(timer);
+                console.log('recaptcha need to be solve')
+            }
+            //else if (count >= 10) {clearInterval(timer);window.location.reload(true)}
+            //clearInterval(timer)
+            return setTimeout(() => {
+                timer(--x)
+            }, 1000)
+        }
     }
-}, 1000)
+    timer(20)
+}
+
+//manual()
+//auto()
+autotimer()
