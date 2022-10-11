a4a814bf8e2abcd804dd41825bf23591360d1539
HARFHO <Harfho77@gmail.com>
2022-10-11 11:01:09 +0100

    Update Dutchycorp_ipBs.user.js

--- Dutchycorp_ipBs.user.js
+++ Dutchycorp_ipBs.user.js
@@ -3,7 +3,11 @@ const scriptTag =`<script defer async>const adBlockDetected_2=()=>{}</script>`;
 const scriptTagappend = document.createRange().createContextualFragment(scriptTag);
 document.querySelector("html").prepend(scriptTagappend);
 document.addEventListener("DOMContentLoaded", function(event) {
-    if (/.*is Linked to your IP.*|what can i do to prevent this in the future|.*Sorry, you have been blocked.*/ig.test(document.body.textContent)) {
+    let text = `is Linked to your IP|
+    what can i do to prevent this in the future|
+    Sorry, you have been blocked|
+    checking if the site connection is secure`
+    if (new RegExp(text,'ig').test(document.body.textContent)) {
         window.location.reload(true)
     }
 });
