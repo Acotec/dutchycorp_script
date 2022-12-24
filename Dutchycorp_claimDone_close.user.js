4e181dd4af240db74af85e32a38aaa9305bbe4fe
HARFHO <Harfho77@gmail.com>
2022-12-24 09:13:01 +0100

    Update Dutchycorp_claimDone_close.user.js

--- Dutchycorp_claimDone_close.user.js
+++ Dutchycorp_claimDone_close.user.js
@@ -1,12 +1,8 @@
- function OnPhone() {
-        0 == GM_getValue("OnPhone", !1) ? GM_setValue("OnPhone", !0) : GM_setValue("OnPhone", !1);
-        window.location.reload()
-    };
-    GM_registerMenuCommand("OnPhone-" + GM_getValue('OnPhone', false), OnPhone, "OnPhone");
-    if("true"==localStorage.getItem("close")&&!/down=/gi.test(window.location)){
-        window.close();window.close();window.close();window.close()
-    }else{
-        if (GM_getValue('OnPhone', false)) {
-            window.close();window.close();window.close();window.close()
-        }
-    }
+function OnPhone() {
+    0 == GM_getValue("OnPhone", !1) ? GM_setValue("OnPhone", !0) : GM_setValue("OnPhone", !1);
+    window.location.reload()
+};
+GM_registerMenuCommand("OnPhone-" + GM_getValue('OnPhone', false), OnPhone, "OnPhone");
+if(("true"==localStorage.getItem("close")|GM_getValue('OnPhone', false))&&!/down=/gi.test(window.location)){
+    window.close();window.close();window.close();window.close()
+}
