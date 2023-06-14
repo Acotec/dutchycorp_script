4dfbd2a2b2e932ac5baa636d332b88deb10cd9b7
HARFHO <Harfho77@gmail.com>
2023-06-14 13:18:06 +0100

    Update Dutchycorp_ipBs.user.js

--- Dutchycorp_ipBs.user.js
+++ Dutchycorp_ipBs.user.js
@@ -1,13 +1,18 @@
-if(/compromised_device.php/ig.test(window.location.href)){window.close()}
+if(/compromised_device.php|524/ig.test(`${window.location.href}-${document.title}`)){window.close()}
 if(/.*ptc.*&_.*/ig.test(window.location.href)){ window.location=window.location.pathname.replace(/.*(undefined|index).*/,'')}
-const scriptTag =`<script defer async>const adBlockDetected_2=()=>{}</script>`;
+const scriptTag =`<script defer async>
+const blocker =()=>{};
+const adBlockDetected_2=()=>{};
+const video_display=()=>{}
+</script>`;
 const scriptTagappend = document.createRange().createContextualFragment(scriptTag);
+document.querySelector("html").append(scriptTagappend);
 document.querySelector("html").prepend(scriptTagappend);
 window.onload = function(){
-    let text = `.*is Linked to your IP.*|
-    .*what can i do to prevent this in the future.*|
+    let text = `.*what can i do to prevent this in the future.*|
     .*Sorry, you have been blocked.*|
     .*checking if the site connection is secure.*`
     if (document.body && new RegExp(text,'ig').test(document.body.textContent)) {
-        window.location=window.location.href}
+        window.location=window.location.href
+    }
 }
