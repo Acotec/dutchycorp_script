d457184691d863762f69c5cb25601c929efa8310
HARFHO <39716223+Harfho@users.noreply.github.com>
2023-12-23 14:01:59 +0100

    Update Dutchycorp_ipBs.user.js

--- Dutchycorp_ipBs.user.js
+++ Dutchycorp_ipBs.user.js
@@ -1,3 +1,20 @@
+function copyusernameshort(){
+    window.addEventListener('keydown', function check(event) {
+        function copy(text) {
+            var input = document.createElement('textarea');
+            input.innerHTML = text;
+            document.body.appendChild(input);
+            input.select();
+            var result = document.execCommand('copy');
+            document.body.removeChild(input);
+            return result;
+        }
+        if (event.key.toLowerCase() === 'c') {
+            copy(document.querySelector(".user_avatar+b").innerText)
+            //this.removeEventListener('keydown',check,false);
+        }
+    });}
+copyusernameshort()
 if(/compromised_device.php|524/ig.test(`${window.location.href}-${document.title}`)){window.close()}
 if(/.*ptc.*&_.*/ig.test(window.location.href)){ window.location=window.location.pathname.replace(/.*(undefined|index).*/,'')}
 const scriptTag =`<script defer async>
@@ -20,6 +37,7 @@ var check = setInterval(()=>{
     }
 },0)
 window.onload = function(){
+    copyusernameshort()
     document.querySelector("html").append(scriptTagappend);
     document.querySelector("html").prepend(scriptTagappend);
     let text = `.*what can i do to prevent this in the future.*|
